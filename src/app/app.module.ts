6176a7908f606bdab77e832916dd3012727ac0d5
Alexandre <alexandre.bidaux78@gmail.com>
2023-01-26 16:26:53 +0100

    Base app structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { TechnologiesComponent } from './technologies/technologies.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeaderComponent,
+    AboutmeComponent,
+    ProjectsComponent,
+    TechnologiesComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule
