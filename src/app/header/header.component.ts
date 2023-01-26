20a124ce0caae0e2a0433e49aa766e07d4b69ccd
unknown <alexandre.bidaux78@gmail.com>
2023-01-26 22:53:52 +0100

    Basic header structure

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  
+  TECHS: string[] = ["Java Springboot", "Java", "VB.NET", "PHP", "JavaScript", "SQL"];
+  tech: string = "";
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  getRandomTech(): string {
+    let idxNewTech = Math.round(Math.random() * (this.TECHS.length) - 1);
+    let newTech = this.TECHS[idxNewTech];
+    return newTech;
+  }
 }
