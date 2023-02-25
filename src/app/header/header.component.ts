08d46359c4d97896c41a92de5f3d17487a927695
Alexandre <alexandre.bidaux78@gmail.com>
2023-02-25 11:44:28 +0100

    Refacto css navbar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  TECHS: string[] = ["Java Springboot", "Java", "VB.NET", "PHP", "JavaScript", "SQL", "C", "C++", "Python", "Angular"];
+  tech: string = "";
+  idxNextTech: number = 0;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.constructNewTech();
+    setInterval(() => {
+      this.constructNewTech();
+    }, 2000)
   }
 
+  async constructNewTech() {
+    let newTech = this.getRandomTech();
+    if (this.tech !== "") this.TECHS.push(this.tech);
+    this.TECHS.splice(this.TECHS.indexOf(newTech), 1)
+    this.tech = "";
+    for(let i = 0; i < newTech.length; i++) {
+      await new Promise(f => setTimeout(f, 50 + Math.random() * 50));
+      this.tech = this.tech + newTech.charAt(i);
+    }
+  }
+  getRandomTech(): string {
+    let idxNewTech = Math.round(Math.random() * (this.TECHS.length - 1));
+    let newTech = this.TECHS[idxNewTech];
+    return newTech;
+  }
 }
